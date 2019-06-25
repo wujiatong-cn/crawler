89d6b6e28a8b676893832b110d9bcddc96884a5a
qihao <qihao01@inspur.com>
2019-06-25 18:40:47 +0800

    <feat>:主页面信息爬取，存储到时序数据库

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -2,8 +2,7 @@
 
 import logging
 from influxdb import InfluxDBClient
-from .utils import check_blank
-from datetime import datetime, timezone
+from .utils import check_blank, str_to_utc_datetime
 from .settings import INFLUX_DB_USER, INFLUX_DB_PWD, INFLUX_DB_HOST, INFLUX_DB_PORT
 # Define your item pipelines here
 #
@@ -28,11 +27,10 @@ class CrawlerPipeline(object):
         unit = item['unit']
         market = item['market']
         price = item['price']
-        date = item['date']
+        date = str_to_utc_datetime(item['date'])
         if check_blank(goods):
             return
-        timestmp = datetime.strptime(date, '%Y/%m/%d %H:%M:%S').timestamp()
-        item_datetime_str = datetime.utcfromtimestamp(timestmp).strftime('%Y-%m-%dT%H:%M:%SZ')
+
         # 发送到数据库
         # 检查数据库是否有记录，没有则需要在record表中记录信息
         json_body = [
@@ -45,11 +43,11 @@ class CrawlerPipeline(object):
                     "unit": unit,
                     "market": market
                 },
-                "time": item_datetime_str,
+                "time": date,
                 "fields": {
                     "price": float(price)
                 }
             }
         ]
-        self.client.write_points(json_body)
+        self.client.write_points(json_body, database=spider.name)
         return item
--- crawler/utils.py
+++ crawler/utils.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime, timezone
 
+
+# 判空
 def check_blank(val):
     if val is None or val == "" or val == " ":
         return True
     return False
+
+
+# 字符串时间转换为UTC时间
+def str_to_utc_datetime(val):
+    timestamp = datetime.strptime(val, '%Y/%m/%d %H:%M:%S').timestamp()
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
